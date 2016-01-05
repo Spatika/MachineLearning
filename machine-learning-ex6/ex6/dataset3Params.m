f630fb4e298c96bd7ac60711832806fa341ad547
Spatika <spatika.n@gmail.com>
2016-01-05 23:50:16 +0530

    Submitted Gaussian with Kernel

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,12 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+values = [0.01 0.03 0.1 0.3 1 3 10 30 ] ; 
 
+error_min = inf ;
 
+for C = values
+  for sigma = values
 
+    [model] = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)) ; 
+    predictions = svmPredict(model, Xval) ; 
+    
+    error = mean(double(predictions ~= yval)) ; 
+    
+    if(error < error_min)
+      C_best = C ;
+      sigma_best = sigma ;
+      error_min = error ;
+    endif 
+    
+  end
+end    
 
-
-
+C = C_best ;
+sigma = sigma_best ; 
+    
 % =========================================================================
 
 end
